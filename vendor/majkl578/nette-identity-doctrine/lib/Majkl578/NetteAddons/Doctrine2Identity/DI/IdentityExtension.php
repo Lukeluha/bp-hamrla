07dbef320988af88b2fe593566daf64cf2903fa6
Lukáš Hamrla <lukas@slever.cz>
2015-03-25 20:05:45 +0100

    vendor folder

---
+++ vendor/majkl578/nette-identity-doctrine/lib/Majkl578/NetteAddons/Doctrine2Identity/DI/IdentityExtension.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Majkl578\NetteAddons\Doctrine2Identity\DI;
+
+use Nette\DI\Compiler;
+use Nette\DI\CompilerExtension;
+use Nette\Configurator;
+use Nette\Loaders\NetteLoader;
+
+// Nette 2.0 & 2.1 compatibility, thx @HosipLan
+if (!class_exists('Nette\DI\CompilerExtension')) {
+	class_alias('Nette\Config\CompilerExtension', 'Nette\DI\CompilerExtension');
+	class_alias('Nette\Config\Compiler', 'Nette\DI\Compiler');
+	class_alias('Nette\Config\Helpers', 'Nette\DI\Config\Helpers');
+}
+if (isset(NetteLoader::getInstance()->renamed['Nette\Configurator']) || !class_exists('Nette\Configurator')) {
+	unset(NetteLoader::getInstance()->renamed['Nette\Configurator']);
+	class_alias('Nette\Config\Configurator', 'Nette\Configurator');
+}
+
+/**
+ * @author Michael Moravec
+ */
+class IdentityExtension extends CompilerExtension
+{
+	const NAME = 'doctrine2identity';
+
+	public function loadConfiguration()
+	{
+		$builder = $this->getContainerBuilder();
+
+		if ($builder->hasDefinition('security.userStorage')) {
+			$builder->getDefinition('security.userStorage')
+					->setFactory('Majkl578\NetteAddons\Doctrine2Identity\Http\UserStorage');
+		} else {
+			$builder->getDefinition('nette.userStorage')
+					->setClass('Majkl578\NetteAddons\Doctrine2Identity\Http\UserStorage');
+		}
+	}
+
+	public static function register(Configurator $configurator)
+	{
+		$configurator->onCompile[] = function (Configurator $sender, Compiler $compiler) {
+			$compiler->addExtension(IdentityExtension::NAME, new IdentityExtension());
+		};
+	}
+}
