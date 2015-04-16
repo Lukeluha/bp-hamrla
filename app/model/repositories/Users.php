9817482dbc398406196dd5c9f89718af5277fd3a
Lukáš Hamrla <lukas@slever.cz>
2015-04-16 18:27:22 +0200

    users in chat fix

--- app/model/repositories/Users.php
+++ app/model/repositories/Users.php
@@ -22,6 +22,7 @@ class Users extends EntityRepository
 	{
 		if ($user->isInRole('teacher')) {
 
+			// all students from my teachings
 			$students = $this->createQueryBuilder()->select('s')
 				->from(Student::getClassName(), 's')
 				->join('s.classes', 'c', 'WITH', 'c.schoolYear = ' . $schoolYear->getId())
@@ -31,12 +32,25 @@ class Users extends EntityRepository
 				->addOrderBy('s.name', 'ASC')
 				->getQuery()->getResult();
 
-			$teachers = $this->createQueryBuilder()
+			// all teachers from my teachings
+			$qb = $this->createQueryBuilder();
+			$teachers = $qb
 				->select('t')
 				->from(Teacher::getClassName(), 't')
 				->join('t.teachings', 'tc')
-				->join('tc.class', 'c', 'WITH', 'c.schoolYear = ' . $schoolYear->getId())
-				->where('t.id != ' . $user->getId())
+				->join('tc.class', 'c')
+				->where( // select all students from all classes in which student participate
+					$qb->expr()->in(
+						'c.id',
+						$this->createQueryBuilder()->select('c2.id')
+							->from(ClassEntity::getClassName(), 'c2')
+							->join('c2.teachings', 'tc2')
+							->join('tc2.teachers', 't2', 'WITH', 't2.id = ' . $user->getId())
+							->where("c2.schoolYear = " . $schoolYear->getId())
+							->getDQL()
+					)
+				)
+				->andWhere('t.id != ' . $user->getId())
 				->addOrderBy('t.surname', 'ASC')
 				->addOrderBy('t.name', 'ASC')
 				->getQuery()->getResult();
@@ -63,22 +77,13 @@ class Users extends EntityRepository
 						)
 						->andWhere('s.id != ' . $user->getId())->getQuery()->getResult();
 
-			$qb3 = $this->createQueryBuilder();
-			$teachers = $qb3
+			$teachers = $this->createQueryBuilder()
 				->select('t')
 				->from(Teacher::getClassName(), 't')
 				->join('t.teachings', 'tc')
 				->join('tc.class', 'c')
-				->where( // select all teachers from all classes in which student participate
-					$qb3->expr()->in(
-						'tc.class',
-						$this->createQueryBuilder()->select('c2.id')
-							->from(ClassEntity::getClassName(), 'c2')
-							->join('c2.students', 's2', 'WITH', 's2.id = ' . $user->getId())
-							->where('c2.schoolYear = ' . $schoolYear->getId())
-							->getDQL()
-					)
-				)->getQuery()->getResult();
+				->join('c.students', 's', 'WITH', 's.id ='. $user->getId())
+				->getQuery()->getResult();
 
 
 			return (array_merge($teachers, $students));
