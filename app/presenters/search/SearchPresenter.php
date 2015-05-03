bab3397c42a4ecedf51396917ad3ad1fbc44b86f
Lukáš Hamrla <lukas@slever.cz>
2015-05-03 23:29:21 +0200

    search

---
+++ app/presenters/search/SearchPresenter.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Presenters;
+
+
+use App\Model\Entities\Question;
+use App\Model\Entities\Task;
+
+class SearchPresenter extends AuthorizedBasePresenter
+{
+	public function actionDefault()
+	{
+		$this->addLinkToNav('Vyhledávání', 'this');
+	}
+
+	public function handleSearch($query)
+	{
+		if (strlen(trim($query))) {
+			$results = array();
+
+			if ($this->user->isInRole('teacher')) {
+				$results['questions'] = $this->em->createQueryBuilder()
+					->select('q')
+					->from(Question::getClassName(), 'q')
+					->join('q.lesson', 'l')
+					->join('l.teaching', 't')
+					->join('t.teachers', 'tc', 'WITH', 'tc.id = ' . $this->user->getId())
+					->where('q.questionText LIKE :query')
+					->setParameter('query', "%".trim($query)."%")
+					->getQuery()->getResult();
+
+				$results['tasks'] = $this->em->createQueryBuilder()
+					->select('t')
+					->from(Task::getClassName(), 't')
+					->join('t.lesson', 'l')
+					->join('l.teaching', 'tc')
+					->join('tc.teachers', 'teacher', 'WITH', 'teacher.id = ' . $this->user->getId())
+					->where('t.taskName LIKE :query')
+					->setParameter('query', "%".trim($query)."%")
+					->getQuery()->getResult();
+
+			} else { // student
+				$results['questions'] = $this->em->createQueryBuilder()
+					->select('q')
+					->from(Question::getClassName(), 'q')
+					->join('q.lesson', 'l')
+					->join('l.teaching', 't')
+					->join('t.class', 'c')
+					->join('c.students', 's', 'WITH', 's.id = ' . $this->user->getId())
+					->where('q.questionText LIKE :query')
+					->setParameter('query', "%".trim($query)."%")
+					->getQuery()->getResult();
+
+				$results['tasks'] = $this->em->createQueryBuilder()
+					->select('t')
+					->from(Task::getClassName(), 't')
+					->join('t.lesson', 'l')
+					->join('l.teaching', 'tc')
+					->join('tc.class', 'c')
+					->join('c.students', 's', 'WITH', 's.id = ' . $this->user->getId())
+					->where('t.taskName LIKE :query')
+					->setParameter('query', "%".trim($query)."%")
+					->getQuery()->getResult();
+			}
+
+			$this->template->results = $results;
+		} else {
+			$this->template->results = null;
+		}
+
+		$this->redrawControl();
+	}
+
+}
